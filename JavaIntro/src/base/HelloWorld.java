d47ae6bf3aacb5fead2763d4da393ba7914b0719
bvachhani <bvachhani@Laptop>
2019-03-26 15:17:34 -0500

    Adding files

---
+++ JavaIntro/src/base/HelloWorld.java
@@ -0,0 +1,11 @@
+package base;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!!! \nThis is basic Java program and run using Jenkins");
+
+	}
+
+}
